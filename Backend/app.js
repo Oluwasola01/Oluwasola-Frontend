16b9d1c782440de679cf59b444bed1b3a2b6940c
Basti12 <seb.knauf1@gmail.com>
2023-01-10 16:17:04 +0100

    added the mongoDB access in the backend folder in the app js file

--- Backend/app.js
+++ Backend/app.js
@@ -9,3 +9,15 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+//access to MongoDB (set-up with MongoDB Atlas)
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://Group6:QZzc4rbmdX8Ab7pv@funtastic-friends.tuwfp0v.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
