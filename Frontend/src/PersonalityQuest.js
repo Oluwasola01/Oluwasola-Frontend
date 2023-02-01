c9171971e2314a76756b3393519609f0468b7bc1
poelki <114226721+poelki@users.noreply.github.com>
2023-02-01 17:02:15 +0100

    added structure to Personality Quest and created StyleSheet

--- Frontend/src/PersonalityQuest.js
+++ Frontend/src/PersonalityQuest.js
@@ -1,5 +1,6 @@
 import react, { useState } from "react"
 
+
 function PersonalityQuest() {
     const [energy, setenergy] = useState(3);
     const [hobbies, sethobbies] = useState();
@@ -8,24 +9,27 @@ function PersonalityQuest() {
         <h1>Share your personality!</h1>
         <section className="energy" >
             <label htmlFor="energy" > Do you get energy from being around other people or do you recharge by being alone?</label>
+            <br />
             <span> I recharge with people</span>
             <input type="range" name="energy" id="energy" min="1" max="5"
                 value={energy}
                 onChange={(event) => { setenergy(event.target.value) }} />
             <span>I need time alone </span>
         </section>
+        <br />
         <section className="hobbies">
             <label htmlFor="hobbies">What do you enjoy doing?</label>
+            <br />
             <select value={hobbies}
                 onChange={(event) => { sethobbies(event.target.value) }}>
                 <option value="Yoga">Yoga</option>
                 <option value="Music">Music</option>
                 <option value="Rugby">Rugby</option>  </select>
         </section>
-
+        <br />
         <section className="extra">
             <label htmlFor="extra">Tell us something about yourself!</label>
-
+            <br />
             <textarea value={extra}
                 onChange={(event) => { setextra(event.target.value) }} id="extra" name="extra" rows="3" cols="40">
 
